55f6a467ad322b10358880f9e2d0f81eb878abee
officialbenz80 <officialbenz80@gmail.com>
2026-06-09 04:57:21 -0700

    Update cookies.ts
    updated cookies file, applied new update

--- app/actions/cookies.ts
+++ app/actions/cookies.ts
@@ -3,27 +3,14 @@
 
 import { cookies } from "next/headers";
 
-// export async function acceptCookies() {
-//   const cookieStore = await cookies();
-
-//   cookieStore.set("cookie_consent", "true", {
-//     maxAge: 60 * 60 * 24 * 365,
-//     path: "/",
-//     secure: process.env.NODE_ENV === "production", // ✅ true on HTTPS
-//     sameSite: "lax",
-//     httpOnly: true, // good practice: no JS access needed for consent
-//   });
-// }
-
-
 export async function acceptCookies() {
   const cookieStore = await cookies();
 
   cookieStore.set("cookie_consent", "true", {
-    maxAge: 60 * 60 * 24 * 365, 
+    maxAge: 60 * 60 * 24 * 365,
     path: "/",
-    secure: true, 
-    sameSite: "lax", // "lax" is generally safe, but "none" requires Secure
-    httpOnly: true,
+    secure: process.env.NODE_ENV === "production", // ✅ true on HTTPS
+    sameSite: "none",
+    httpOnly: true, // good practice: no JS access needed for consent
   });
 }
